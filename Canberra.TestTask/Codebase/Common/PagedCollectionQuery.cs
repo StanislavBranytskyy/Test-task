d0fb1d1696e0fdcaa6908e3b133e9b52e8aae6da
sbranytskyy <guardian.stas@gmail.com>
2016-06-06 09:56:53 +0300

    Update PagedCollectionQuery.cs

--- Canberra.TestTask/Codebase/Common/PagedCollectionQuery.cs
+++ Canberra.TestTask/Codebase/Common/PagedCollectionQuery.cs
@@ -16,11 +16,11 @@ namespace Canberra.TestTask.Codebase.Common
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            if (Sorting != null && (Sorting.Direction != SortDirection.Ascending || Sorting.Direction != SortDirection.Descending))
+            if (Sorting != null && (Sorting.Direction != SortDirection.Ascending && Sorting.Direction != SortDirection.Descending))
             {
                 yield return new ValidationResult("Sort order is unacceptable");
             }
-            else if (Sorting != null && (Sorting.Property != "FullName" || Sorting.Property != "Gender"))
+            else if (Sorting != null && (Sorting.Property != "FullName" && Sorting.Property != "Gender"))
             {
                 yield return new ValidationResult(string.Format("Field {0} is not acceptable", Sorting.Property));
             }
